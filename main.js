298c9ed0b1b7cbd98495369053475651ff155838
Junz2025 <junzec@outlook.com>
2025-07-04 23:07:15 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-// Placeholder JS file for Pygbag build
+
+// Simulated loader for main.py compiled to WASM
+console.log("Loading 金庸群侠传 游戏...");
+document.body.insertAdjacentHTML("beforeend", "<p style='text-align:center;'>[游戏引擎启动中... 这只是演示版]</p>");
